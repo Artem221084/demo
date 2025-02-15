d7275b29b876f1df81c2aaf031990f64ef03d41a
Artem <artem221084@mail.ru>
2025-02-15 22:01:26 +0300

    apg

--- game.py
+++ game.py
@@ -1,20 +1,25 @@
+"""Игра угадай число.
+Компьютер сам загадывает и угадывает число
+"""
 import numpy as np
 
-number = np.random.randint(1, 101) # загадываем число
-count = 0
+def random_predict(number:int=1) -> int:
+    """Рандомно угадываем число
 
-while True:
-    count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
 
-    if predict_number > number:
-        print("Число должно быть меньше!")
+    Returns:
+        int: Число попыток
+    """
 
-    elif predict_number < number:
-        print("Число должно быть больше!")
+    count = 0
 
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break # конец игры, выход из цикла
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101) # предполагаемое число
+        if number == predict_number:
+            break # выход из цикла, если угадали
+    return(count)
 
 print(f'Количество попыток: {random_predict()}')
